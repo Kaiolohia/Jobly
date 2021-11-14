b9459ac432c944868ee7d79a89f67c05201cc000
TruxSux <kaithomasdean@outlook.com>
2021-11-13 22:00:18 -1000

    Added admin checks

--- routes/users.js
+++ routes/users.js
@@ -5,8 +5,8 @@
 const jsonschema = require("jsonschema");
 
 const express = require("express");
-const { ensureLoggedIn } = require("../middleware/auth");
-const { BadRequestError } = require("../expressError");
+const { ensureLoggedIn, ensureAdmin } = require("../middleware/auth");
+const { BadRequestError, UnauthorizedError } = require("../expressError");
 const User = require("../models/user");
 const { createToken } = require("../helpers/tokens");
 const userNewSchema = require("../schemas/userNew.json");
@@ -27,7 +27,7 @@ const router = express.Router();
  * Authorization required: login
  **/
 
-router.post("/", ensureLoggedIn, async function (req, res, next) {
+router.post("/", ensureLoggedIn, ensureAdmin, async function (req, res, next) {
   try {
     const validator = jsonschema.validate(req.body, userNewSchema);
     if (!validator.valid) {
@@ -51,7 +51,7 @@ router.post("/", ensureLoggedIn, async function (req, res, next) {
  * Authorization required: login
  **/
 
-router.get("/", ensureLoggedIn, async function (req, res, next) {
+router.get("/", ensureLoggedIn, ensureAdmin, async function (req, res, next) {
   try {
     const users = await User.findAll();
     return res.json({ users });
@@ -70,6 +70,9 @@ router.get("/", ensureLoggedIn, async function (req, res, next) {
 
 router.get("/:username", ensureLoggedIn, async function (req, res, next) {
   try {
+    if(!(res.locals.user.username === req.params.username || res.locals.user.isAdmin)) {
+      throw new UnauthorizedError("Not your account.")
+    }
     const user = await User.get(req.params.username);
     return res.json({ user });
   } catch (err) {
@@ -90,6 +93,9 @@ router.get("/:username", ensureLoggedIn, async function (req, res, next) {
 
 router.patch("/:username", ensureLoggedIn, async function (req, res, next) {
   try {
+    if(!(res.locals.user.username === req.params.username || res.locals.user.isAdmin)) {
+      throw new UnauthorizedError("Not your account.")
+    }
     const validator = jsonschema.validate(req.body, userUpdateSchema);
     if (!validator.valid) {
       const errs = validator.errors.map(e => e.stack);
@@ -111,6 +117,9 @@ router.patch("/:username", ensureLoggedIn, async function (req, res, next) {
 
 router.delete("/:username", ensureLoggedIn, async function (req, res, next) {
   try {
+    if(!(res.locals.user.username === req.params.username || res.locals.user.isAdmin)) {
+      throw new UnauthorizedError("Not your account.")
+    }
     await User.remove(req.params.username);
     return res.json({ deleted: req.params.username });
   } catch (err) {
